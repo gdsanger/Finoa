b02c14ce0f9ec797e06f6683559cb21f027c93d2
Christian Angermeier <ca@perculasoft.de>
2025-11-22 14:41:47 +0100

    we

--- finoa/settings.py
+++ finoa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=2ps@qmi0hie-$-p_8+c6rnqp+)rlnq5oos0oq&3w0rm#3$l)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "192.168.178.155", "localhost", "127.0.0.1"]
 
 
 # Application definition
